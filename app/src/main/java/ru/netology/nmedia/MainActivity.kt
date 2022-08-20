65b76f82f5d73655c1d3d7d3226d68833dcffe6d
Daria Galichiy <daria-galichii@yandex.ru>
2022-08-20 19:00:12 +0300

    RecyclerView, ListAdapter implemented

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -3,15 +3,12 @@ package ru.netology.nmedia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.annotation.DrawableRes
+import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.functions.likesToText
 import ru.netology.nmedia.viewModel.PostViewModel
 
-class MainActivity : AppCompatActivity() {
 
-    private val viewModel by viewModels<PostViewModel>()
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,29 +16,18 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.data.observe(this){ post ->
-            binding.render(post)
-        }
-
-        binding.postPage.likeBtn.setOnClickListener {
-            viewModel.onLikeClicked()
-        }
-
-        binding.postPage.shareBtn.setOnClickListener {
-            viewModel.onShareClicked()
+        val viewModel: PostViewModel by viewModels()
+        val adapter = PostsAdapter(
+            onLikeClicked = { post ->
+                viewModel.onLikeClicked(post)
+            },
+            onShareClicked = { post ->
+                viewModel.onShareClicked(post)
+            }
+        )
+        binding.postsRecyclerView.adapter = adapter
+        viewModel.data.observe(this) { posts ->
+            adapter.submitList(posts)
         }
     }
-
-    private fun ActivityMainBinding.render(post: Post) {
-        postPage.authorName.text = post.author
-        postPage.postText.text = post.content
-        postPage.date.text = post.published
-        postPage.likeBtn.setImageResource(getLikeIconResId(post.likedByUser))
-        postPage.likesCount.text = likesToText(post.likes)
-        postPage.sharesCount.text = likesToText(post.shares)
-    }
-
-    @DrawableRes
-    private fun getLikeIconResId(liked: Boolean) =
-        if (liked) R.drawable.ic_like_pushed_24dp else R.drawable.ic_like_24dp
 }
